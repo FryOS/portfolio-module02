24452f3d4385984cd42a7a45dfcc00945455a2c8
Alexey Ospishchev <alexeyosp@gmail.com>
2022-11-02 00:20:50 +0300

    header

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "normalize.css";
+import "./styles/main.scss";
+import "./index.html";
+
+
+
+// function openModal(){
+//   document.getElementById("dropdown").style.display = "block";
+// }
+
+// function closeModal(){
+//   document.getElementById("dropdown").style.display = "-400px";
+// }
+
