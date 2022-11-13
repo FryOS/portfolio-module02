cc15d2a4cad92b0c7926c7c8aa250b7eeef44150
Alexey Ospishchev <alexeyosp@gmail.com>
2022-11-14 00:15:57 +0300

    Mobile 141122

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "./index.html";
 
 
 
-// function openModal(){
-//   document.getElementById("dropdown").style.display = "block";
-// }
+function openModal(){
+  document.getElementById("modal-menu").style.display = "block";
+}
 
-// function closeModal(){
-//   document.getElementById("dropdown").style.display = "-400px";
-// }
+function closeModal(){
+  document.getElementById("dropdown").style.display = "-400px";
+}
 
