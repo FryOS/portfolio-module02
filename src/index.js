7530fc95997e1e2a447e21fbc8dd2d4520dc5cb0
Ospishchev Alexey <alexeyosp@gmail.com>
2022-12-05 21:15:47 +0300

    dropdown

--- src/index.js
+++ src/index.js
@@ -12,3 +12,25 @@ function closeModal(){
   document.getElementById("dropdown").style.display = "-400px";
 }
 
+document.querySelector(".dropdown__button").addEventListener("click", function(event){
+  event.preventDefault();
+  document.querySelector(".dropdown__list").classList.toggle("dropdown__list-visible")
+})
+
+
+document.querySelectorAll(".dropdown__list-item").forEach(function(listItem){
+  listItem.addEventListener("click", function(e){
+    e.stopPropagation();
+    document.querySelector(".dropdown__button").innerText = this.innerText;
+    document.querySelector(".dropdown__button").focus();
+    document.querySelector(".dropdown__list").classList.remove("dropdown__list-visible")
+  })
+})
+
+
+document.addEventListener("click", function(e){
+  if(e.target !== document.querySelector(".dropdown__button")){
+    console.log(e.target)
+    document.querySelector(".dropdown__list").classList.remove("dropdown__list-visible")
+  }
+})
